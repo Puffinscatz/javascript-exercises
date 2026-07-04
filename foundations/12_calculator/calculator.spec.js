e57a1232c3066c4d144aff6846bbdcd31fc52d0f
Puffinscatz <robbiethomas1001@gmail.com>
2026-07-04 12:02:04 +0100

    answered questions

--- foundations/12_calculator/calculator.spec.js
+++ foundations/12_calculator/calculator.spec.js
@@ -5,31 +5,31 @@ describe('add', () => {
     expect(calculator.add(0, 0)).toBe(0);
   });
 
-  test.skip('adds 2 and 2', () => {
+  test('adds 2 and 2', () => {
     expect(calculator.add(2, 2)).toBe(4);
   });
 
-  test.skip('adds positive numbers', () => {
+  test('adds positive numbers', () => {
     expect(calculator.add(2, 6)).toBe(8);
   });
 });
 
 describe('subtract', () => {
-  test.skip('subtracts numbers', () => {
+  test('subtracts numbers', () => {
     expect(calculator.subtract(10, 4)).toBe(6);
   });
 
-  test.skip('subtracts negative numbers', () => {
+  test('subtracts negative numbers', () => {
     expect(calculator.subtract(-10, -4)).toBe(-6);
   });
 
-  test.skip('subtracts numbers of mixed parity', () => {
+  test('subtracts numbers of mixed parity', () => {
     expect(calculator.subtract(-8, 7)).toBe(-15);
   });
 });
 
 describe('sum', () => {
-  test.skip('computes the sum of an empty array', () => {
+  test('computes the sum of an empty array', () => {
     expect(calculator.sum([])).toBe(0);
   });
 
@@ -47,17 +47,17 @@ describe('sum', () => {
 });
 
 describe('multiply', () => {
-  test.skip('multiplies two numbers', () => {
+  test('multiplies two numbers', () => {
     expect(calculator.multiply([2, 4])).toBe(8);
   });
 
-  test.skip('multiplies several numbers', () => {
+  test('multiplies several numbers', () => {
     expect(calculator.multiply([2, 4, 6, 8, 10, 12, 14])).toBe(645120);
   });
 });
 
 describe('power', () => {
-  test.skip('raises one number to the power of another number', () => {
+  test('raises one number to the power of another number', () => {
     expect(calculator.power(4, 3)).toBe(64); // 4 to third power is 64
   });
 
@@ -71,11 +71,11 @@ describe('factorial', () => {
     expect(calculator.factorial(0)).toBe(1); // 0! = 1
   });
 
-  test.skip('computes the factorial of 1', () => {
+  test('computes the factorial of 1', () => {
     expect(calculator.factorial(1)).toBe(1);
   });
 
-  test.skip('computes the factorial of 2', () => {
+  test('computes the factorial of 2', () => {
     expect(calculator.factorial(2)).toBe(2);
   });
 
